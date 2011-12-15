777b70046ae9d302d6253aed66d77177a1d6210a
Ansgar Esztermann <aeszter@gwdg.de>
2011-12-15 13:53:42 +0100

    Remove obsolete src/main.adb
    superceded by src/lightsout.adb

--- src/main.adb
+++
@@ -1,8 +0,0 @@
-with Node_Groups;
-with Config;
-
-procedure Lightsout is
-   All_Nodes : constant Node_Groups.List := Config.Read;
-begin
-   All_Nodes.Iterate (Node_Groups.Manage'Access);
-end Lightsout;
