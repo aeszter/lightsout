f0799cbcaa6b14580a428e79599e48e0a72c23b8
Ansgar Esztermann <aeszter@gwdg.de>
2012-12-04 10:48:03 +0100

    Refactor: Combine Actions.Enable and Actions.Disable
    These subprograms used to be identical except for the qmod
    parameter (-e vs -d).

--- src/actions.adb
+++ src/actions.adb
@@ -10,22 +10,38 @@ with Ada.Exceptions; use Ada.Exceptions;
 package body Actions is
 
    procedure Activate_Power_Switch (The_Node : String; Command : String);
+   procedure Disable_Or_Enable (The_Node : Nodes.Node; Enable : Boolean);
 
-   procedure Enable (What : Nodes.Node) is
+
+
+   procedure Disable_Or_Enable (The_Node : Nodes.Node; Enable : Boolean) is
+
       PID          : Process_ID;
       Return_Value : Termination_Status;
       Args         : POSIX.POSIX_String_List;
       Template     : Process_Template;
-      The_Node     : constant String := Get_Name (What);
+      Node_Name     : constant String := Get_Name (What => The_Node);
    begin
-      if Utils.Dry_Run (Message => "enabling " & The_Node,
+      if Enable then
+         if Utils.Dry_Run (Message => "enabling " & Node_Name,
                        Show_On_Verbose => False) then
-         return;
+            return;
+         end if;
+      else
+         if Utils.Dry_Run (Message => "disabling " & Node_Name,
+                       Show_On_Verbose => False) then
+            return;
+         end if;
       end if;
       Append (Args, "qmod");
-      Append (Args, "-e");
-      Append (Args, To_POSIX_String ("*@" & The_Node));
-      Debug ("enabling " & The_Node);
+      if Enable then
+         Append (Args, "-e");
+         Debug ("enabling " & Node_Name);
+      else
+         Append (Args, "-d");
+         Debug ("disabling " & Node_Name);
+      end if;
+      Append (Args, To_POSIX_String ("*@" & Node_Name));
       Open_Template (Template);
       Set_File_Action_To_Close (Template => Template,
                                 File     => POSIX.IO.Standard_Output);
@@ -41,44 +57,27 @@ package body Actions is
          when others => raise Subcommand_Error with "qmod exited with status" & Exit_Status_Of (Return_Value)'Img;
       end case;
    exception
-         when E : POSIX_Error =>
-      raise Subcommand_Error with "qmod raised error when called with ""-e *@" & The_Node &
-      """:" & Exception_Message (E);
+      when E : POSIX_Error =>
+         if Enable then
+            raise Subcommand_Error with "qmod raised error when called with ""-e *@" & Node_Name &
+            """:" & Exception_Message (E);
+         else
+            raise Subcommand_Error with "qmod raised error when called with ""-d *@" & Node_Name &
+            """:" & Exception_Message (E);
+         end if;
+   end Disable_Or_Enable;
+
+
+   procedure Enable (What : Nodes.Node) is
+   begin
+      Disable_Or_Enable (The_Node => What,
+                            Enable   => True);
    end Enable;
 
+
    procedure Disable (What : Nodes.Node) is
-      PID          : Process_ID;
-      Return_Value : Termination_Status;
-      Args         : POSIX.POSIX_String_List;
-      Template     : Process_Template;
-      The_Node     : constant String := Get_Name (What);
    begin
-      if Utils.Dry_Run (Message => "disabling " & The_Node,
-                       Show_On_Verbose => False) then
-         return;
-      end if;
-      Append (Args, "qmod");
-      Append (Args, "-d");
-      Append (Args, To_POSIX_String ("*@" & The_Node));
-      Debug ("disabling " & The_Node);
-      Open_Template (Template);
-      Set_File_Action_To_Close (Template => Template,
-                                File     => POSIX.IO.Standard_Output);
-      Start_Process (Child    => PID,
-                     Template => Template,
-                     Pathname => "/cm/shared/apps/sge/current/bin/lx26-amd64/qmod",
-                     Arg_List => Args);
-      Wait_For_Child_Process (Status => Return_Value, Child => PID);
-      case Exit_Status_Of (Return_Value) is
-         when Normal_Exit => return;
-         when Failed_Creation_Exit => raise Subcommand_Error with "Failed to create qmod process";
-         when Unhandled_Exception_Exit => raise Subcommand_Error with "Unhandled exception in qmod";
-         when others => raise Subcommand_Error with "qmod exited with status" & Exit_Status_Of (Return_Value)'Img;
-      end case;
-      exception
-      when E : POSIX_Error =>
-      raise Subcommand_Error with "qmod raised error when called with ""-d *@" & The_Node &
-      """:" & Exception_Message (E);
+      Disable_Or_Enable (The_Node => What, Enable => False);
    end Disable;
 
    ---------------------------
