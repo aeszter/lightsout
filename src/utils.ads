49d304439ea93aff48c07533289cdc8057a94767
Ansgar Esztermann <aeszter@gwdg.de>
2013-05-22 15:14:40 +0200

    version bump

--- src/utils.ads
+++ src/utils.ads
@@ -4,7 +4,7 @@ use Ada.Strings.Unbounded;
 with Ada.Numerics.Float_Random;
 
 package Utils is
-   Version : String := "v1.0";
+   Version : String := "feature/bugzilla";
 
    package String_Lists is new Doubly_Linked_Lists (Element_Type => Unbounded_String);
    subtype String_List is String_Lists.List;
