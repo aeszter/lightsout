808b42b562a7267d09838799ea431ed51a33a4eb
Ansgar Esztermann <aeszter@gwdg.de>
2013-05-23 16:00:39 +0200

    SOAP call finished -- this seems to be a dead end
    AWS does not provide XML/rpc, and Bugzilla does not accept SOAP.
    There does not seem to be an Ada implementation of XML/rpc on the web. Maybe
    we should create our own? Or start with something like adazilla?

--- src/bugzilla.adb
+++ src/bugzilla.adb
@@ -1,6 +1,11 @@
 with AWS.Client;
-with AWS.Response;
+with AWS.Response; use AWS.Response;
+with SOAP.Message.Payload;
+with SOAP.Client;
 with Ada.Text_IO; use Ada.Text_IO;
+with SOAP.Message.Response;
+with SOAP.Parameters;
+with SOAP.Types;
 
 package body Bugzilla is
 
@@ -9,8 +14,15 @@ package body Bugzilla is
    ----------
 
    procedure Test is
+      Params : constant SOAP.Parameters.List := SOAP.Parameters."+" (SOAP.Types.N);
+      Payload : constant SOAP.Message.Payload.Object := SOAP.Message.Payload.Build ("Bugzilla.version", Params);
+      Response : constant SOAP.Message.Response.Object'Class := SOAP.Client.Call ("http://ram.mpibpc.intern/bugzilla/xmlrpc.cgi", Payload);
+      Replied  : constant SOAP.Parameters.List := SOAP.Message.Parameters (Response);
    begin
       Put_Line (AWS.Response.Message_Body (AWS.Client.Get (URL => "http://ram.mpibpc.intern/bugzilla")));
+      for I in 1 .. SOAP.Parameters.Argument_Count (Replied) loop
+         Put_Line (SOAP.Types.Image (SOAP.Parameters.Argument (P => Replied, N => I)));
+      end loop;
    end Test;
 
 end Bugzilla;
