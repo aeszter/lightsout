8fad49ccb5e7df35301c3dc59e1725fab52dfe4b
Ansgar Esztermann <aeszter@gwdg.de>
2014-07-01 10:22:58 +0200

    Better exception handling
    mainly for Bug #1305

--- src/actions.adb
+++ src/actions.adb
@@ -97,28 +97,53 @@ package body Actions is
       Return_Value : Termination_Status;
 
    begin
-      Debug (cmsh_Command);
-      Append (Args, "cmsh");
-      Append (Args, "-c");
-      Append (Args, To_POSIX_String (cmsh_Command));
-      Open_Template (Template);
-      Set_File_Action_To_Close (Template => Template,
-                                File     => POSIX.IO.Standard_Output);
-      Start_Process_Search (Child    => PID,
-                            Template => Template,
-                            Filename => "cmsh",
-                            Arg_List => Args);
-      Wait_For_Child_Process (Status => Return_Value, Child => PID);
-      case Exit_Status_Of (Return_Value) is
-         when Normal_Exit => return;
-         when Failed_Creation_Exit => raise Subcommand_Error with "Failed to create cmsh process";
-         when Unhandled_Exception_Exit => raise Subcommand_Error with "Unhandled exception in cmsh";
-         when others => raise Subcommand_Error with "cmsh exited with status" & Exit_Status_Of (Return_Value)'Img;
+      begin
+         Debug (cmsh_Command);
+         Append (Args, "cmsh");
+         Append (Args, "-c");
+         Append (Args, To_POSIX_String (cmsh_Command));
+         Open_Template (Template);
+         Set_File_Action_To_Close (Template => Template,
+                                   File     => POSIX.IO.Standard_Output);
+         exception
+         when others =>
+            Verbose_Message ("in cmsh setup:");
+            raise;
+      end;
+      begin
+         Start_Process_Search (Child    => PID,
+                               Template => Template,
+                               Filename => "cmsh",
+                               Arg_List => Args);
+      exception
+         when others =>
+            Verbose_Message ("in cmsh start:");
+            raise;
+      end;
+      begin
+         Wait_For_Child_Process (Status => Return_Value, Child => PID);
+      exception
+         when others =>
+            Verbose_Message ("in cmsh wait:");
+            raise;
+      end;
+      case Termination_Cause_Of (Return_Value) is
+         when Exited =>
+            case Exit_Status_Of (Return_Value) is
+               when Normal_Exit => return;
+               when Failed_Creation_Exit => raise Subcommand_Error with "Failed to create cmsh process";
+               when Unhandled_Exception_Exit => raise Subcommand_Error with "Unhandled exception in cmsh";
+               when others => raise Subcommand_Error with "cmsh exited with status" & Exit_Status_Of (Return_Value)'Img;
+            end case;
+         when Terminated_By_Signal =>
+            Verbose_Message ("cmsh terminated by signal " & Termination_Signal_Of (Return_Value)'Img);
+         when Stopped_By_Signal =>
+            Verbose_Message ("cmsh stopped");
       end case;
    exception
       when E : POSIX_Error =>
          raise Subcommand_Error with "cmsh raised error when called with """
-           & cmsh_Command & """:" & Exception_Message (E);
+           & cmsh_Command & """:" & Exception_Information (E);
    end Activate_Power_Switch;
 
    procedure Poweron (What : Nodes.Node) is
